baf95e2dda8dcf2cfc67925e170022f089bfcd2c
DevNacho <ignacio.barbeyto@gmail.com>
2024-05-20 22:40:32 +0200

    Primer commit

---
+++ PruebaTecnicaCells_IgnacioBarbeyto/prueba-tecnica-cells-ignaciobarbeyto/app/pages/create-product-page/create-product-page.js
@@ -0,0 +1,169 @@
+/* eslint-disable no-unused-expressions */
+import { html } from 'lit-element';
+import { CellsPage } from '@cells/cells-page';
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import '@bbva-web-components/bbva-core-collapse/bbva-core-collapse.js';
+import '@bbva-web-components/bbva-core-image/bbva-core-image.js';
+import '@bbva-web-components/bbva-foundations-grid-tools-layout/bbva-foundations-grid-tools-layout.js';
+import '@bbva-web-components/bbva-web-button-default/bbva-web-button-default.js';
+import '@bbva-web-components/bbva-web-form-checkbox/bbva-web-form-checkbox.js';
+import '@bbva-web-components/bbva-web-form-fieldset/bbva-web-form-fieldset.js';
+import '@bbva-web-components/bbva-web-form-radio-button/bbva-web-form-radio-button.js';
+import '@bbva-web-components/bbva-web-form-text/bbva-web-form-text.js';
+import '@bbva-web-components/bbva-web-header-public-web/bbva-web-header-public-web.js';
+import '@bbva-web-components/bbva-web-module-footer/bbva-web-module-footer-language-list-item.js';
+import '@bbva-web-components/bbva-web-module-footer/bbva-web-module-footer.js';
+import '@bbva-web-components/bbva-web-panel-outstanding-opportunity/bbva-web-panel-outstanding-opportunity.js';
+import '@cells-demo/demo-data-dm/demo-data-dm.js';
+import '@cells-demo/demo-web-template/demo-web-template.js';
+import '@bbva-web-components/bbva-web-form-amount/bbva-web-form-amount.js';
+import styles from './create-product-page-styles.js';
+
+const DEFAULT_I18N_KEYS = {
+  formHeading: 'create-product-page.form-heading',
+  labelInput1: 'create-product-page.form-input-1-label',
+  labelInput2: 'create-product-page.form-input-2-label',
+  labelInput3: 'create-product-page.form-input-3-label',
+  labelButton: 'create-product-page.form-button',
+};
+
+/* eslint-disable new-cap */
+class CreateProductPage extends BbvaCoreIntlMixin(CellsPage) {
+  static get is() {
+    return 'create-product-page';
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Custom keys for default texts
+       */
+      i18nKeys: {
+        type: Object,
+        attribute: false,
+      },
+
+      product: {
+        type: Object,
+        attribute: false,
+      }
+    };
+  }
+
+  constructor() {
+    super();
+    this.product = {};
+    this.i18nKeys = {};
+  }
+
+  static get styles() {
+    return [ styles ];
+  }
+
+  firstUpdated(props) {
+    super.firstUpdated && super.firstUpdated(props);
+
+    const queryScope = this.shadowRoot || this;
+    this._dm = queryScope.querySelector('demo-data-dm');
+    this._form = queryScope.querySelector('form');
+    window.IntlMsg.lang = localStorage.getItem('language') || 'en-US';
+  }
+
+  update(props) {
+    if (props.has('i18nKeys')) {
+      this._i18nKeys = { ...DEFAULT_I18N_KEYS, ...this.i18nKeys };
+    }
+
+    super.update && super.update(props);
+  }
+
+  render() {
+    return html` 
+      <demo-web-template
+        page-title="Create Product"
+      >
+        <div slot="app-top-content">
+
+        </div>
+
+        <div slot="app-main-content">
+          ${this._formProductTpl}
+          
+        </div>
+
+        <div slot="app-main-content" data-grid="full-width">
+
+        </div>
+
+        <demo-data-dm @settings="${this._getUserSettings}"></demo-data-dm>
+      </demo-web-template>
+    `;
+  }
+
+
+  get _formProductTpl() {
+    return html`
+        <form enctype="multipart/form-data">
+
+          <h2>${this.t(this._i18nKeys.formHeading)}</h2>
+          <bbva-web-form-text id="name" label="${this.t(this._i18nKeys.labelInput1)}"></bbva-web-form-text>
+          <bbva-web-form-amount id="amount" label="${this.t(this._i18nKeys.labelInput2)}"></bbva-web-form-amount>
+          <!--<bbva-core-image id="image" label="${this.t(this._i18nKeys.labelInput3)}"></bbva-core-image>-->
+          
+          <bbva-web-button-default
+            id="send"
+            type="button" 
+            @click="${this._addProduct}"
+          >
+            ${this.t(this._i18nKeys.labelButton)}
+          </bbva-web-button-default>
+        </form>
+    `;
+  }
+
+  _addProduct(ev) {
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    const form = document.querySelector('#cells-template-create-product').shadowRoot.querySelector('form');
+
+    // Asegúrate de que el formulario existe
+    if (!form) {
+      console.error('Formulario no encontrado');
+      return;
+    }
+
+    // Valor del campo de entrada con name
+    const inputName = form.querySelector('#name');
+    const productName = inputName ? inputName.value : '';
+
+    // Valor del campo de entrada con amount
+    const inputAmount = form.querySelector('#amount');
+    const productAmount = inputAmount ? inputAmount.value : '';
+
+    // Valor del campo de entrada con image
+    //const inputImage = form.querySelector('#image');
+    //const productImage = inputImage ? inputImage.value : '';
+
+    // Asegúrate de que todos los valores necesarios están presentes
+    if (!productName || !productAmount /*|| !productImage*/) {
+      console.error('Todos los campos son obligatorios');
+      return;
+    }
+
+    const details = {
+      nameP: productName,
+      amountP: productAmount,
+
+      //imageP: productImage
+    };
+
+    // Publica el evento
+    this.publish('add_product', details);
+
+    // Navega a la página del listado
+    this.navigate('list-product');
+  }
+
+}
+window.customElements.define(CreateProductPage.is, CreateProductPage);
---
+++ PruebaTecnicaCells_IgnacioBarbeyto/prueba-tecnica-cells-ignaciobarbeyto/app/pages/list-product-page/list-product-page.js
@@ -0,0 +1,169 @@
+/* eslint-disable no-unused-expressions */
+import { CellsPage } from '@cells/cells-page';
+import { html } from 'lit-element';
+import { bbvaCopy, bbvaEdit, bbvaHelp, bbvaTasks, bbvaEmail, bbvaBuilding, bbvaFeedback } from '@bbva-web-components/bbva-foundations-icons';
+import { BbvaCoreIntlMixin } from '@bbva-web-components/bbva-core-intl-mixin';
+import '@bbva-web-components/bbva-foundations-grid-tools-layout/bbva-foundations-grid-tools-layout.js';
+import '@bbva-web-components/bbva-web-amount/bbva-web-amount.js';
+import '@bbva-web-components/bbva-web-card-product/bbva-web-card-product.js';
+import '@bbva-web-components/bbva-web-badge-default/bbva-web-badge-default.js';
+import '@bbva-web-components/bbva-web-button-default/bbva-web-button-default.js';
+import '@bbva-web-components/bbva-web-expandable-accordion/bbva-web-expandable-accordion.js';
+import '@bbva-web-components/bbva-web-form-text/bbva-web-form-text.js';
+import '@bbva-web-components/bbva-web-form-checkbox/bbva-web-form-checkbox.js';
+import '@bbva-web-components/bbva-web-clip-entities/bbva-web-clip-entities.js';
+import '@bbva-web-components/bbva-web-form-select-filter/bbva-web-form-select-filter.js';
+import '@bbva-web-components/bbva-web-list-item-definition-amount/bbva-web-list-item-definition-amount.js';
+import '@bbva-web-components/bbva-web-notification-message/bbva-web-notification-message.js';
+import '@bbva-web-components/bbva-web-progress-bar/bbva-web-progress-bar.js';
+import '@bbva-web-components/bbva-web-table-filter/bbva-web-table-filter.js';
+import '@cells-demo/demo-data-dm/demo-data-dm.js';
+import '@cells-demo/demo-web-template/demo-web-template.js';
+import '@cells-demo/demo-table-movements/demo-table-movements.js';
+import styles from './list-product-page-styles.js';
+
+const iconset = {
+  copy: bbvaCopy(),
+  edit: bbvaEdit()
+};
+
+const HEADER_ICONS = [{
+  icon: bbvaHelp(),
+  label: 'Help'
+}, {
+  icon: bbvaTasks(),
+  label: 'Task'
+}, {
+  icon: bbvaEmail(),
+  label: 'Email',
+  notifications: 0
+}, {
+  icon: bbvaBuilding(),
+  label: 'Building'
+}, {
+  icon: bbvaFeedback(),
+  label: 'Feedback'
+}];
+
+const DEFAULT_I18N_KEYS = {
+  accountDetail: 'dashboard-page.account-detail',
+  accountMovements: 'dashboard-page.account-movements',
+  accountNumber: 'dashboard-page.account-number',
+  accountType: 'dashboard-page.account-type',
+  alias: 'dashboard-page.alias',
+  allowableLimit: 'dashboard-page.allowable-limit',
+  arranged: 'dashboard-page.arranged',
+  arrangedAllowableLimit: 'dashboard-page.arranged-allowable-limit',
+  availableBalance: 'dashboard-page.available-balance',
+  businessName: 'dashboard-page.business-name',
+  copyAccountNumber: 'dashboard-page.copy-account-number',
+  currency: 'dashboard-page.currency',
+  difference: 'dashboard-page.difference',
+  editAlias: 'dashboard-page.edit-alias',
+  entity: 'dashboard-page.entity',
+  selectAccount: 'dashboard-page.select-account',
+  totalAccountBalance: 'dashboard-page.total-account-balance',
+};
+
+/* eslint-disable new-cap */
+class ListProductPage extends BbvaCoreIntlMixin(CellsPage) {
+  static get is() {
+    return 'list-product-page';
+  }
+
+  static get properties() {
+    return {
+      /**
+       * Custom keys for default texts
+       */
+      i18nKeys: {
+        type: Object,
+        attribute: false,
+      },
+    };
+  }
+
+  constructor() {
+    super();
+    this.i18nKeys = {};
+  }
+
+  connectedCallback() {
+    super.connectedCallback && super.connectedCallback();
+
+  }
+
+  firstUpdated(props) {
+    super.firstUpdated && super.firstUpdated(props);
+
+    const queryScope = this.shadowRoot || this;
+    this._dm = queryScope.querySelector('demo-data-dm');
+  }
+
+  update(props) {
+    if (props.has('i18nKeys')) {
+      this._i18nKeys = { ...DEFAULT_I18N_KEYS, ...this.i18nKeys };
+    }
+
+    super.update && super.update(props);
+  }
+
+  onPageEnter() {
+    this.subscribe('add_product', (ev) => {
+      this._product = ev;
+    });
+    this._handleAddProduct(this._product);
+  }
+
+  static get styles() {
+    return [ styles ];
+  }
+
+  render() {
+    return html`
+      <demo-web-template
+        page-title="List Products"
+      >
+        <div class="main" slot="app-top-content">
+        <h1> Listado de productos </h1>
+        </div>
+        
+        <div class="main" slot="app-main-content">
+          ${this._renderCardProduct()}
+        </div>
+
+        <demo-data-dm
+        ></demo-data-dm>
+
+      </demo-web-template>
+    `;
+  }
+
+  _renderCardProduct() {
+    this._products = JSON.parse(localStorage.getItem('productos')) || [];
+    return html`
+    ${this._products.map((product, index) => html`
+      <bbva-web-card-product
+      badge-text="Producto ${(index + 1)}"
+      launch-text="Nombre: ${(product.nameP)}"
+      preheading="Precio: ${(product.amountP)} €"> 
+      </bbva-web-card-product>
+    `)}
+  `;
+  }
+
+  _handleAddProduct() {
+    const { nameP, amountP/*, imageP*/} = this._product;
+
+    if (this._product) {
+      const existingProducts = JSON.parse(localStorage.getItem('productos')) || [];
+      const newProduct = { nameP, amountP/*, imageP*/};
+
+      existingProducts.push(newProduct);
+      localStorage.setItem('productos', JSON.stringify(existingProducts));
+    }
+  }
+
+}
+
+window.customElements.define(ListProductPage.is, ListProductPage);
